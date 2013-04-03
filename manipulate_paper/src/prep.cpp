1f69cef26cfeda107b06edb0b87faf39ab29a532
Ross Hanson <reh237@cornell.edu>
2013-04-03 06:23:43 -0400

    Everything works, just need to figure out how close I can get the hand

--- manipulate_paper/src/prep.cpp
+++ manipulate_paper/src/prep.cpp
@@ -7,25 +7,35 @@
 int main(int argc, char **argv){
   ros::init (argc, argv, "move_arm_pose_goal_test");
   ros::NodeHandle nh;
-  actionlib::SimpleActionClient<arm_navigation_msgs::MoveArmAction> move_arm("move_right_arm",true);
-  move_arm.waitForServer();
+  actionlib::SimpleActionClient<arm_navigation_msgs::MoveArmAction> move_arm_right("move_right_arm",true);
+  actionlib::SimpleActionClient<arm_navigation_msgs::MoveArmAction> move_arm_left("move_left_arm",true);
+  move_arm_right.waitForServer();
+  move_arm_left.waitForServer();
   ROS_INFO("Connected to server");
-  arm_navigation_msgs::MoveArmGoal goalA;
+  arm_navigation_msgs::MoveArmGoal goalRight;
+  arm_navigation_msgs::MoveArmGoal goalLeft;
 
-  goalA.motion_plan_request.group_name = "right_arm";
-  goalA.motion_plan_request.num_planning_attempts = 1;
-  goalA.motion_plan_request.planner_id = std::string("");
-  goalA.planner_service_name = std::string("ompl_planning/plan_kinematic_path");
-  goalA.motion_plan_request.allowed_planning_time = ros::Duration(5.0);
+  goalRight.motion_plan_request.group_name = "right_arm";
+  goalRight.motion_plan_request.num_planning_attempts = 1;
+  goalRight.motion_plan_request.planner_id = std::string("");
+  goalRight.planner_service_name = std::string("ompl_planning/plan_kinematic_path");
+  goalRight.motion_plan_request.allowed_planning_time = ros::Duration(5.0);
+
+  goalLeft.motion_plan_request.group_name = "left_arm";
+  goalLeft.motion_plan_request.num_planning_attempts = 1;
+  goalLeft.motion_plan_request.planner_id = std::string("");
+  goalLeft.planner_service_name = std::string("ompl_planning/plan_kinematic_path");
+  goalLeft.motion_plan_request.allowed_planning_time = ros::Duration(5.0);
   
   arm_navigation_msgs::SimplePoseConstraint desired_pose;
+  arm_navigation_msgs::SimplePoseConstraint desired_post_left;
   desired_pose.header.frame_id = "torso_lift_link";
   desired_pose.link_name = "r_wrist_roll_link";
-  desired_pose.pose.position.x = 0.75;
-  desired_pose.pose.position.y = -0.188;
+  desired_pose.pose.position.x = 0;
+  desired_pose.pose.position.y = -0.75;
   desired_pose.pose.position.z = 0;
 
-  desired_pose.pose.orientation.x = 0.5;
+  desired_pose.pose.orientation.x = 0.0;
   desired_pose.pose.orientation.y = 0.0;
   desired_pose.pose.orientation.z = 0.0;
   desired_pose.pose.orientation.w = 1.0;
@@ -38,27 +48,60 @@ int main(int argc, char **argv){
   desired_pose.absolute_pitch_tolerance = 0.04;
   desired_pose.absolute_yaw_tolerance = 0.04;
   
-  arm_navigation_msgs::addGoalConstraintToMoveArmGoal(desired_pose,goalA);
+  arm_navigation_msgs::SimplePoseConstraint desired_post_left_left;
+  desired_post_left.header.frame_id = "torso_lift_link";
+  desired_post_left.link_name = "l_wrist_roll_link";
+  desired_post_left.pose.position.x = 0;
+  desired_post_left.pose.position.y = 0.75;
+  desired_post_left.pose.position.z = 0;
+
+  desired_post_left.pose.orientation.x = 0.0;
+  desired_post_left.pose.orientation.y = 0.0;
+  desired_post_left.pose.orientation.z = 0.0;
+  desired_post_left.pose.orientation.w = 1.0;
+
+  desired_post_left.absolute_position_tolerance.x = 0.02;
+  desired_post_left.absolute_position_tolerance.y = 0.02;
+  desired_post_left.absolute_position_tolerance.z = 0.02;
+
+  desired_post_left.absolute_roll_tolerance = 0.04;
+  desired_post_left.absolute_pitch_tolerance = 0.04;
+  desired_post_left.absolute_yaw_tolerance = 0.04;
+  
+  
 
   if (nh.ok())
   {
     bool finished_within_time = false;
-    move_arm.sendGoal(goalA);
-    finished_within_time = move_arm.waitForResult(ros::Duration(200.0));
+    arm_navigation_msgs::addGoalConstraintToMoveArmGoal(desired_pose,goalRight);
+    arm_navigation_msgs::addGoalConstraintToMoveArmGoal(desired_post_left,goalLeft);
+   move_arm_right.sendGoal(goalRight);
+   move_arm_left.sendGoal(goalLeft);
+    finished_within_time = move_arm_left.waitForResult(ros::Duration(200.0));
+    finished_within_time = move_arm_right.waitForResult(ros::Duration(200.0));    
     if (!finished_within_time)
     {
-      move_arm.cancelGoal();
+      move_arm_left.cancelGoal();
+      move_arm_right.cancelGoal();
       ROS_INFO("Timed out achieving goal A");
     }
     else
     {
-      actionlib::SimpleClientGoalState state = move_arm.getState();
-      bool success = (state == actionlib::SimpleClientGoalState::SUCCEEDED);
+      actionlib::SimpleClientGoalState state_right = move_arm_right.getState();
+      actionlib::SimpleClientGoalState state_left = move_arm_left.getState();
+      bool success = (state_right == actionlib::SimpleClientGoalState::SUCCEEDED);
       if(success)
-        ROS_INFO("Action finished: %s",state.toString().c_str());
+        ROS_INFO("Action finished: %s",state_right.toString().c_str());
       else
-        ROS_INFO("Action failed: %s",state.toString().c_str());
+        ROS_INFO("Action failed: %s",state_right.toString().c_str());
+      success = (state_left == actionlib::SimpleClientGoalState::SUCCEEDED);
+      if(success)
+        ROS_INFO("Action finished: %s",state_left.toString().c_str());
+      else
+        ROS_INFO("Action failed: %s",state_left.toString().c_str());
     }
   }
+
+  
   ros::shutdown();
 }
--- manipulate_paper/src/segment_table.cpp
+++ manipulate_paper/src/segment_table.cpp
@@ -13,8 +13,21 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/ModelCoefficients.h>
+#include <pcl_ros/transforms.h>
+//Rviz marker
+#include <visualization_msgs/Marker.h>
+//Transform
+#include <tf/transform_listener.h>
+//Math
+#include <cmath>
+//My stuff
+#include <manipulate_paper/movements.h>
+
 ros::Publisher pub;
+ros::Publisher marker_pub;
 ros::Subscriber sub;
+ros::NodeHandle* nh;
+std::string target_id = "/base_footprint";
 
 using namespace std;
 
@@ -22,10 +35,78 @@ bool cmp(const pcl::PointXYZ &point1, const pcl::PointXYZ &point2){
 return point1.x < point2.x;
 }
 
+void add_rviz_marker(pcl::PointXYZ point_to_mark, pcl::PointXYZ point_to_line, string frame_id){
+  ros::Rate r(1);
+
+
+  // Set our initial shape type to be a cube
+  uint32_t shape = visualization_msgs::Marker::CUBE;
+
+  visualization_msgs::Marker marker, line;
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = frame_id; line.header.frame_id = frame_id;
+  marker.header.stamp = ros::Time::now(); line.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "basic_shapes"; line.ns="basic_shapes";
+  marker.id = 0; line.id = 1;
+
+    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker.type = shape;  line.type = visualization_msgs::Marker::LINE_STRIP;
+
+
+  line.scale.x = .05;
+  
+    // Set the marker action.  Options are ADD and DELETE
+    marker.action = line.action = visualization_msgs::Marker::ADD;
+    geometry_msgs::Point point1;
+    geometry_msgs::Point point2;
+    point1.x = point_to_mark.x; point2.x = point_to_line.x;
+    point1.y = point_to_mark.y; point2.y = point_to_line.y;
+    point1.z = point_to_mark.z; point2.z = point_to_line.z;
+    line.points.push_back(point1);
+    line.points.push_back(point2);
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = point_to_mark.x;
+    marker.pose.position.y = point_to_mark.y;
+    marker.pose.position.z = point_to_mark.z;
+    line.pose.orientation.x = marker.pose.orientation.x = 0.0;
+    line.pose.orientation.y = marker.pose.orientation.y = 0.0;
+    line.pose.orientation.z = marker.pose.orientation.z = 0.0;
+    line.pose.orientation.w = marker.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = .2;
+    marker.scale.y = .2;
+    marker.scale.z = .2;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 0.0f; line.color.r = 1.0f;
+    marker.color.g = 1.0f; line.color.g = 1.0f;
+    marker.color.b = 0.0f; line.color.b = 0.0f;
+    marker.color.a = 1.0; line.color.a = 1.0;
+
+    marker.lifetime = ros::Duration(); line.lifetime = ros::Duration();
+
+    // Publish the marker
+    marker_pub.publish(marker); marker_pub.publish(line);
+  }
+
+
+void perform_arm_motion(float table_edge_x, float table_edge_z){
+  float x_offset = 0.01;
+  float z_offset = 0.05;
+  cerr << "Shooting for: " << (table_edge_x - x_offset) << " and " << (table_edge_z - z_offset);
+  Movements::move_left_gripper(nh,table_edge_x-x_offset, 0.0, table_edge_z-z_offset);
+  
+  
+}
+
 void 
 cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud)
 {
-
+  cerr << "Frame id " <<cloud->header.frame_id;
 sensor_msgs::PointCloud2 cloud_filtered_blob, out_blob, tmp_out_blob;
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>), cloud_p (new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
 pcl::PointCloud<pcl::PointXYZ> best_out;
@@ -33,7 +114,15 @@ pcl::PointCloud<pcl::PointXYZ> best_out;
 //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new plc::PointCloud<pcl::PointXYZ>), cloud_p (new plc::PointCloud<pcl::PointXYZ>), cloud_f (new plc::PointCloud<pcl::PointXYZ>);
   // Perform the actual filtering
 
-pcl::fromROSMsg(*cloud,*cloud_filtered);
+
+tf::TransformListener listener;
+
+ ros::Time now = ros::Time::now();
+ listener.waitForTransform(target_id,cloud->header.frame_id,now,ros::Duration(3.0));
+  tf::StampedTransform transform;
+  listener.lookupTransform(target_id,cloud->header.frame_id,ros::Time::now()-ros::Duration(1),transform);
+ pcl_ros::transformPointCloud(target_id, transform, *cloud,tmp_out_blob);
+pcl::fromROSMsg(tmp_out_blob,*cloud_filtered); 
 
 pcl::PCDWriter writer;
 
@@ -50,7 +139,7 @@ seg.setMaxIterations(1000);
 seg.setDistanceThreshold(0.01);
 
 pcl::ExtractIndices<pcl::PointXYZ> extract;
-float best_val = -100;
+float best_val = 100;
 
 int i = 0, nr_points = (int) cloud_filtered->points.size();
 while (cloud_filtered->points.size() > .04 * nr_points && i<10)
@@ -71,8 +160,8 @@ std:cerr << "Model coefficients: " << coefficients->values[0] << " " << coeffici
 std::stringstream ss;
 ss << "table_scene_lms_" << i << ".pcd";
 writer.write<pcl::PointXYZ> (ss.str (), *cloud_p, false);
-if (coefficients->values[3]>best_val){
-best_val = coefficients->values[3];
+ if (coefficients->values[3]<best_val){
+   best_val = coefficients->values[3];
 best_out = *cloud_p;
 }
 extract.setNegative(true);
@@ -97,11 +186,9 @@ pcl::fromROSMsg(out_blob,best_out);
 std::sort(best_out.begin(),best_out.end(),cmp);
 std::cerr << "Lowest x position: " << best_out.points[0].x << std::endl;
 
-
+ add_rviz_marker(best_out.points[0], best_out.points[100],target_id);
 ROS_INFO("FINISHED RUNNING");
-
-
-
+ perform_arm_motion(best_out.points[0].x,best_out.points[0].z);
 sub.shutdown();
 }
 
@@ -112,13 +199,13 @@ main (int argc, char** argv)
 {
   // Initialize ROS
   ros::init (argc, argv, "my_pcl_tutorial");
-  ros::NodeHandle nh;
+  nh = new ros::NodeHandle;
 
   // Create a ROS subscriber for the input point cloud
-sub  = nh.subscribe ("input", 1, cloud_cb);
+sub  = nh->subscribe ("input", 1, cloud_cb);
   // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
-
+  pub = nh->advertise<sensor_msgs::PointCloud2> ("output", 1);
+  marker_pub = nh->advertise<visualization_msgs::Marker>("visualization_marker", 1);
   // Spin
   ros::spin ();
 }
